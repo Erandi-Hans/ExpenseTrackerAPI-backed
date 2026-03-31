d629b8e8aa2fdcfe3399ad1b10be5e9f8b4c1cac
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-03-31 16:58:31 +0530

    text with postman

--- Program.cs
+++ Program.cs
@@ -1,24 +1,27 @@
-﻿using ExpenseTrackerAPI_backed.Data; 
+﻿using ExpenseTrackerAPI_backed.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Connection String එක ලබාගැනීම
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
 
+// DbContext එක Register කිරීම (Retry logic එකත් සමඟ)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString, sqlOptions =>
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null)));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-  
 }
 
 app.UseHttpsRedirection();
