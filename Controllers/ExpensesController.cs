023c8c4a549c869a8ee78c21f28d0e581bddfb7f
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-03-31 15:54:02 +0530

    implement controller and data folders

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ExpenseTrackerAPI_backed.Data;
+using ExpenseTrackerAPI_backed.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerAPI_backed.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly AppDbContext _context; // Entity Framework පාවිච්චි කරලා
+
+        public ExpensesController(AppDbContext _context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
+        {
+            return await _context.Expenses.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Expense>> PostExpense(Expense expense)
+        {
+            _context.Expenses.Add(expense);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetExpenses), new { id = expense.Id }, expense);
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseTrackerAPI_backed.Model; // ඔයාගේ Model folder එකේ නම මෙතන තියෙන්න ඕනේ
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerAPI_backed.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
